ea2a2fcfc4e835d44ad81dabe4331eee3dd0330f
Antimoxs <tblockcrafter@gmail.com>
2022-05-28 01:34:00 +0200

    Updated version

--- src/main/java/dev/antimoxs/hyplus/HyPlus.java
+++ src/main/java/dev/antimoxs/hyplus/HyPlus.java
@@ -37,8 +37,8 @@ public class HyPlus extends LabyModAddon {
      */
     private static HyPlus instance;
 
-    private static final String VERSION = "1.0.0beta";
-    private static final String LASTUPDATED = "30. April 2022";
+    private static final String VERSION = "1.0.4";
+    private static final String LASTUPDATED = "28. May 2022";
     public HyAbout hyAbout = new HyAbout(
             new kvp("Initial Release", 3)
     );
